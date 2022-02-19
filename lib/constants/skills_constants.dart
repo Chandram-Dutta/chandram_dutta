0119050049a8b26fdf4ea3f4e18fa76a229722f5
Chandram-Dutta <chandramdutta2004@gmail.com>
2022-02-19 11:35:10 +0530

    Update

--- lib/constants/skills_constants.dart
+++ lib/constants/skills_constants.dart
@@ -1,6 +1,15 @@
 List<String> skillsConstants = [
   """
 Flutter is a UI framework for building applications for mobile, web, and desktop from a single codebase.
+I have used Flutter for building mobile apps and web apps.
+I started using Flutter in 2019.
+I also started Teaching Flutter as FlutterZed on Youtube and Instagram since 2021.
+Topics Mastered in Flutter:
+◼ Basic Dart
+◼ Useful Flutter Widget
+◼ Cross Platform Development Packages like fluent_ui, macos_ui, yaru, Material & Cupertino.
+◼ State Management using Riverpod
+◼ Using Flutter to build apps for mobile, web, and desktop.
 """,
   '''
 Dart is a language for building high-performance, cross-platform, and scalable applications.'''
