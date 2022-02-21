2225cc2595072d212e9dc422a8c83f447a0ca9c6
Chandram-Dutta <chandramdutta2004@gmail.com>
2022-02-21 11:07:47 +0530

    Update

--- lib/constants/skills_constants.dart
+++ lib/constants/skills_constants.dart
@@ -12,5 +12,19 @@ Topics Mastered in Flutter:
 ◼ Using Flutter to build apps for mobile, web, and desktop.
 """,
   '''
-Dart is a language for building high-performance, cross-platform, and scalable applications.'''
+Dart is a language for building high-performance, cross-platform, and scalable applications.
+I have used Dart for building mobile apps and web apps using Flutter.
+I started using Dart in 2019.
+I can perform basic extensive operations using Dart.'''
+'''
+Firebase is a cloud platform for hosting, managing, and scaling developer applications.
+I have used Firebase with Flutter(FlutterFire) for building mobile apps and web apps.
+I have also use Firebase Hosting Services
+I can perform basic CRUD Operations using Firebase.
+I can alos use Firebase Authentication and Cloud Firestore.'''
+'''
+Linux is a open source operating system kernel.
+I have been using Linux based Distributions like Ubuntu, Fedora, etc as my daily driver since 2020.
+ 
+'''
 ];
