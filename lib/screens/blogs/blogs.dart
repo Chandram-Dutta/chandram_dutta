0dab70824e0553a429d0cf3213f8dc2660e034d3
Chandram-Dutta <chandramdutta2004@gmail.com>
2022-03-21 11:42:48 +0530

    Update

--- lib/screens/blogs/blogs.dart
+++ lib/screens/blogs/blogs.dart
@@ -10,127 +10,143 @@ class BlogsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Blogs'),
-          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-          elevation: 0,
-          actions: [ThemeIconButton()],
-        ),
-        body: Row(
-          children: [
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SizedBox(
-                  height: 200,
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          Color.fromARGB(255, 92, 92, 92)),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0),
+      appBar: AppBar(
+        title: Text('Blogs'),
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        elevation: 0,
+        actions: [ThemeIconButton()],
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    height: 200,
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            Color.fromARGB(255, 92, 92, 92)),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
                         ),
                       ),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Blogs On",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          SizedBox(width: 10),
+                          FaIcon(
+                            FontAwesomeIcons.medium,
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                      onPressed: () {
+                        launchUrl("https://www.instagram.com/flutter.zed/");
+                      },
                     ),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Blogs On",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        SizedBox(width: 10),
-                        FaIcon(
-                          FontAwesomeIcons.medium,
-                          color: Colors.white,
-                        ),
-                      ],
-                    ),
-                    onPressed: () {
-                      launchUrl("https://www.instagram.com/flutter.zed/");
-                    },
                   ),
                 ),
               ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SizedBox(
-                  height: 200,
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.white),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    height: 200,
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.white),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
                         ),
                       ),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Blogs On",
+                            style:
+                                TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                          ),
+                          SizedBox(width: 10),
+                          SizedBox(
+                            height: 25,
+                            width: 25,
+                            child: Image.asset("assets/icons/hashnode.png"),
+                          )
+                        ],
+                      ),
+                      onPressed: () {
+                        launchUrl("https://www.instagram.com/flutter.zed/");
+                      },
                     ),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Blogs On",
-                          style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
-                        ),
-                        SizedBox(width: 10),
-                        SizedBox(
-                          height: 25,
-                          width: 25,
-                          child: Image.asset("assets/icons/hashnode.png"),
-                        )
-                      ],
-                    ),
-                    onPressed: () {
-                      launchUrl("https://www.instagram.com/flutter.zed/");
-                    },
                   ),
                 ),
               ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SizedBox(
-                  height: 200,
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          Color.fromARGB(255, 255, 0, 0)),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    height: 200,
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            Color.fromARGB(255, 255, 0, 0)),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
                         ),
                       ),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Blogs On",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          SizedBox(width: 10),
+                          FaIcon(
+                            FontAwesomeIcons.dev,
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                      onPressed: () {
+                        launchUrl("https://www.instagram.com/flutter.zed/");
+                      },
                     ),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Blogs On",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        SizedBox(width: 10),
-                        FaIcon(
-                          FontAwesomeIcons.dev,
-                          color: Colors.white,
-                        ),
-                      ],
-                    ),
-                    onPressed: () {
-                      launchUrl("https://www.instagram.com/flutter.zed/");
-                    },
                   ),
                 ),
               ),
+            ],
+          ),
+          Card(
+            child: ListTile(
+              title: Text("Introduction to Riverpod"),
+              subtitle: Text("Medium"),
+              trailing: Icon(Icons.arrow_forward_ios),
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
