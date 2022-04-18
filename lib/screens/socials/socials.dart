7df58ab3edb59ad49494e157979f4030b716c8c1
Chandram-Dutta <chandramdutta2004@gmail.com>
2022-04-18 17:02:35 +0530

    update

--- lib/screens/socials/socials.dart
+++ lib/screens/socials/socials.dart
@@ -1,4 +1,6 @@
+import 'package:chandram_dutta/screens/home/widgets/theme_icon_button.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SocialsPage extends StatelessWidget {
   const SocialsPage({Key? key}) : super(key: key);
@@ -6,7 +8,23 @@ class SocialsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("Social"),
+        actions: [
+          ThemeIconButton(),
+        ],
+      ),
+      body: Wrap(children: [
+        MaterialButton(
+          child: Row(
+            children: [
+              FaIcon(FontAwesomeIcons.instagram),
+              Text("Instagram"),
+            ],
+          ),
+          onPressed: () {},
+        )
+      ]),
     );
   }
 }
